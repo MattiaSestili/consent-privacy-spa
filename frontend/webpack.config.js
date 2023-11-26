2fa4eb24832573745894ccd6cb815dd44cb99e08
mattsestili <mattia.sestili@xerini.co.uk>
2023-11-26 12:24:06 +0000

    - wrong eslint settings - missing html lang

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,35 +1,35 @@
-const prod = process.env.NODE_ENV === 'production';
+import HtmlWebpackPlugin from "html-webpack-plugin"
+import MiniCssExtractPlugin from "mini-css-extract-plugin"
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const prod = process.env.NODE_ENV === "production"
 
 module.exports = {
-    mode: prod ? 'production' : 'development',
-    entry: './src/index.tsx',
-    output: {
-        path: __dirname + '/dist/',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/,
-                exclude: /node_modules/,
-                resolve: {
-                    extensions: ['.ts', '.tsx', '.js', '.json'],
-                },
-                use: 'ts-loader',
-            },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
-            },
-        ]
-    },
-    devtool:'eval-source-map',
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
-        new MiniCssExtractPlugin(),
-    ],
-};
+  mode: prod ? "production" : "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: __dirname + "/dist/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: [".ts", ".tsx", ".js", ".json"]
+        },
+        use: "ts-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
+  },
+  devtool: "eval-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html"
+    }),
+    new MiniCssExtractPlugin()
+  ]
+}
